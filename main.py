38d5a9576512dfb8b63c20cdff7263a5754814ed
Mr. Funny <yom8travel@gmail.com>
2020-04-06 03:17:15 -0700

    User can now open random sauce in default browser
    The user is given the choice to open a random sauce from "sauce.txt" in their default web browser (will not open in a private/incognito window, however)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import os
+import webbrowser
 
 def sauceAmount(amount):
 	while True:
@@ -18,12 +19,29 @@ def generateSauce():
 		sauce = random.randint(100000, 299999) #There is some sauce that are >299999 but they're uncommon
 		print(sauce)
 		output.write(str(sauce) + "\n") #Writes the generated sauce to seperate lines in a .txt file
+	output.close()
 	print("WARNING: Keep in mind this is all random, some of the sauce may not work")
 	print("There should be a .txt file with the sauce generated where this .py file is")
+	while True:
+		while True:
+			answer = input("Open random sauce in default web browser? WARNING: It will not open in a private window (y/n): ")
+			if answer in ("y", "n"):
+				break
+			print("Please type y or n (case sensitive)")
+		if answer == "y":
+			outputRead = open("sauce.txt").read().splitlines()
+			randomOpen = random.choice(outputRead)
+			print("Opening " + str(randomOpen) + " in your default web browser...")
+			webbrowser.open("nhentai.net/g/" + str(randomOpen), new=2)
+			break
+		else:
+			break
 
 #MAIN PROGRAM
 sauceAmount = sauceAmount("How much sauce do you want? ")
 if sauceAmount > 200000:
 	print("Whoa, that's too much sauce, you'll get repeats and that's no good")
+elif sauceAmount <= 0:
+	print("Exiting...")
 else:
 	generateSauce()

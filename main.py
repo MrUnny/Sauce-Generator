7a476be0bfe68158be13fb2445c655076d496a76
Mr. Funny <yom8travel@gmail.com>
2020-04-06 12:26:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,6 @@ sauceAmount = sauceAmount("How much sauce do you want? ")
 if sauceAmount > 200000:
 	print("Whoa, that's too much sauce, you'll get repeats and that's no good")
 elif sauceAmount <= 0:
-	print("Exiting...")
+	print("Number that is <= 0. Exiting...")
 else:
 	generateSauce()
